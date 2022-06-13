3b908ec713f4958610f50a8dd5ab849b236f8d7f
Alex <avicente@watchity.com>
2022-06-13 15:52:17 +0200

    Fix Proton Steam folder

---
+++ fixProtonFolder.sh
@@ -0,0 +1,5 @@
+sudo rm -R ~/.steam/steam/steamapps/compatdata;
+
+sudo rm -R /media/gamedisk/SteamLibrary/steamapps/compatdata; 
+
+ln -s ~/.steam/steam/steamapps/compatdata /media/gamedisk/SteamLibrary/steamapps/;
